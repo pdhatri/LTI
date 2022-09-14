3a5c89813ae90219ef64edf482295148412b69e8
Padakanti Dhatri <padakantidhatri@Padakantis-MacBook-Pro.local>
2022-09-14 12:16:56 -0600

    Added parameters and retrieved to display

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Created views for display of content when user clicks on the link.
 def index(request):
-    return HttpResponse("Welcome to Computer Education Class")
+    #retrieved course id and user name to display
+    return HttpResponse("Welcome user: "+request.POST["custom_canvas_course_id"]+" to the course with id: "+ request.POST["custom_canvas_user_id"])
